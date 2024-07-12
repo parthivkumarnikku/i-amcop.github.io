db0ee6392330cf317329d4a85a3089a2ef55dbb5
parthiv kumar <cb.en.u4cys22046@cb.students.amrita.edu>
2024-07-12 10:27:53 +0530

    major_update

--- ui/js/response.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("first_prompt").innerHTML = 5 + 6;
-const responseObj = {
-  hello: "Hey ! How are you doing ?",
-  hey: "Hey! What's Up",
-  
-  today: new Date().toDateString(),
-  time: new Date().toLocaleTimeString(),
-};
